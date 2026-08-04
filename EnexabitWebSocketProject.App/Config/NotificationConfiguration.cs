b918d57a11b66fda97f70e2dd3c738696f6c603a
lamonaDev <aymanmohamed.programmer@gmail.com>
2026-08-04 07:05:56 +0300

    add: notification model configuration

---
+++ EnexabitWebSocketProject.App/Config/NotificationConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using EnexabitWebSocketProject.App.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EnexabitWebSocketProject.App.Config;
+
+public class NotificationConfiguration : IEntityTypeConfiguration<Notification>
+{
+    public void Configure(EntityTypeBuilder<Notification> builder)
+    {
+        builder.HasKey(n => n.Id);
+
+        builder.Property(n => n.Title)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(n => n.Data)
+            .HasMaxLength(1000);
+
+        builder.Property(n => n.Type)
+            .IsRequired();
+
+        builder.Property(n => n.IsRead)
+            .HasDefaultValue(false);
+
+        builder.Property(n => n.IsAcknowledged)
+            .HasDefaultValue(false);
+
+        builder.Property(n => n.IsSystemWide)
+            .HasDefaultValue(false);
+
+        builder.HasIndex(n => n.UserId);
+        builder.HasIndex(n => n.CreatedAt);
+        builder.HasIndex(n => new { n.UserId, n.IsRead });
+
+        builder.HasOne(n => n.User)
+            .WithMany()
+            .HasForeignKey(n => n.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
