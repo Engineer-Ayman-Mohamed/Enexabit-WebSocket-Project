591a8626fc32b0006edbbe3e5927569dd28eebd8
lamonaDev <aymanmohamed.programmer@gmail.com>
2026-08-04 06:13:01 +0300

    add: notification types enum

---
+++ EnexabitWebSocketProject.App/Enums/NotificationTypes.cs
@@ -0,0 +1,16 @@
+﻿namespace EnexabitWebSocketProject.App.Enums;
+
+public enum NotificationTypes
+{
+    /// <summary>User was @mentioned in a channel message.</summary>
+    MentionUser = 0,
+
+    /// <summary>Channel name or settings were updated.</summary>
+    ChannelUpdate = 1,
+
+    /// <summary>System maintenance is scheduled.</summary>
+    SystemMaintenance = 2,
+
+    /// <summary>System-wide announcement from admin.</summary>
+    SystemAnnouncement = 3
+}
