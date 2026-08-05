6e2344b7d41f58354b4d3245415bac29a97b8750
lamonaDev <aymanmohamed.programmer@gmail.com>
2026-08-05 07:28:01 +0300

    add: UnreadNotificationsCountResponse.cs DTO

---
+++ EnexabitWebSocketProject.App/DTOs/UnreadNotificationsCountResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace EnexabitWebSocketProject.App.DTOs;
+
+/// <summary>Response for unread count endpoint.</summary>
+public class UnreadNotificationsCountResponse
+{
+    /// <summary>Number of unread notifications.</summary>
+    public int Count { get; set; }
+}
